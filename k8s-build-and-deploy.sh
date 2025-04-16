037ae4ab6571be7f3154caf6485730ab80ad2aee
Budi Santoso <sebuah.email@gmail.com>
2025-04-16 17:36:15 +0700

    [patch]k8s files -update Kubernetes deployment script for improved clarity and structure

--- k8s-build-and-deploy.sh
+++ k8s-build-and-deploy.sh
@@ -6,12 +6,12 @@ set -e
 # Define variables
 APP_NAME="simulator-outstation-dnp3-dynamic"
 DOCKERFILE="DOCKERFILE"
-REGISTRY_IP="1.2.3.4"
-REGISTRY_PORT=""
-NAMESPACE="scada"
-DEPLOYMENT_FILE="k8s-deploy.yaml"
-FULL_IMAGE_NAME="$REGISTRY_IP/$NAMESPACE/$APP_NAME"
+REGISTRY_PATH="1.2.3.4/scada"
+NAMESPACE="scada-grita"
+DEPLOY_FILE="k8s-deploy.yaml"
+FULL_IMAGE_NAME="$REGISTRY_PATH/$APP_NAME"
 
+### DOCKER BUILD
 # Login to Docker
 echo "Logging into Docker..."
 docker login || { echo "Docker login failed!"; exit 1; }
@@ -26,7 +26,7 @@ docker images | grep $APP_NAME || { echo "Image not found!"; exit 1; }
 
 # Tag the image for the remote repository
 if [ -n "$REGISTRY_PORT" ]; then
-    FULL_IMAGE_NAME="$REGISTRY_IP:$REGISTRY_PORT/$NAMESPACE/$APP_NAME"
+    FULL_IMAGE_NAME="$REGISTRY_PATH/$APP_NAME"
 fi
 
 echo "Tagging image as $FULL_IMAGE_NAME..."
@@ -36,18 +36,17 @@ docker tag $APP_NAME:latest $FULL_IMAGE_NAME || { echo "Docker tag failed!"; exi
 echo "Pushing image to registry..."
 docker push $FULL_IMAGE_NAME || { echo "Docker push failed!"; exit 1; }
 
-# Delete the existing deployment if it exists
-if kubectl get deployment simulator-outstation-dnp3 -n "$NAMESPACE" &> /dev/null; then
-    echo "Deleting existing deployment..."
-    kubectl delete deployment simulator-outstation-dnp3 -n "$NAMESPACE"
-fi
+### KUBERNETES DEPLOYMENT
+# Delete the existing deployment
+echo "Deleting existing deployment..."
+kubectl delete -f $DEPLOY_FILE --ignore-not-found
 
 # Apply the Kubernetes deployment
 echo "Applying Kubernetes deployment..."
-kubectl apply -f $DEPLOYMENT_FILE || { echo "Kubernetes deployment failed!"; exit 1; }
+kubectl apply -f $DEPLOY_FILE || { echo "Kubernetes deployment failed!"; exit 1; }
 
 # Display deployed resources
-echo "Deployment and Service status:"
+echo "Deployed Resources status:"
 kubectl get all -n "$NAMESPACE" | grep $APP_NAME
 
 echo "Deployment completed successfully!"
