834f1f91145eb7842f58ce91cb17b86f59e8f999
Budi Santoso <sebuah.email@gmail.com>
2025-04-16 16:34:16 +0700

    [patch]socket -remove unused dependency & code

--- src/main/java/dev/boenkkk/simulator_outstation_dnp3_dynamic/scheduler/OutstationScheduler.java
+++ src/main/java/dev/boenkkk/simulator_outstation_dnp3_dynamic/scheduler/OutstationScheduler.java
@@ -1,12 +1,10 @@
 package dev.boenkkk.simulator_outstation_dnp3_dynamic.scheduler;
 
 import dev.boenkkk.simulator_outstation_dnp3_dynamic.service.OutstationsService;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @EnableScheduling
