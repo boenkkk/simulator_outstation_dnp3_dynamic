7957b5ce03a65f765783aefa14e118260ef078d6
Budi Santoso <sebuah.email@gmail.com>
2025-06-23 14:25:01 +0700

    [patch]TapChanger -add props for status in binary input

--- src/main/java/dev/boenkkk/simulator_outstation_dnp3_dynamic/model/TapChangerModel.java
+++ src/main/java/dev/boenkkk/simulator_outstation_dnp3_dynamic/model/TapChangerModel.java
@@ -18,11 +18,20 @@ public class TapChangerModel {
     @JsonProperty("index_bo_command_raise_lower")
     private Integer indexBoCommandRaiseLower;
 
+    @JsonProperty("index_bi_raise_lower")
+    private Integer indexBiRaiseLower;
+
     @JsonProperty("index_bo_command_auto_manual")
     private Integer indexBoCommandAutoManual;
 
+    @JsonProperty("index_bi_auto_manual")
+    private Integer indexBiAutoManual;
+
     @JsonProperty("index_bo_command_local_remote")
     private Integer indexBoCommandLocalRemote;
+
+    @JsonProperty("index_bi_local_remote")
+    private Integer indexBiLocalRemote;
 
     @JsonProperty("interval_scheduler")
     private Integer intervalScheduler;
@@ -36,11 +45,20 @@ public class TapChangerModel {
     @JsonProperty("value")
     private Integer value;
 
+    @JsonProperty("value_command_raise_lower")
+    private Boolean valueCommandRaiseLower;
+
     @JsonProperty("value_raise_lower")
     private Boolean valueRaiseLower;
 
+    @JsonProperty("value_command_auto_manual")
+    private Boolean valueCommandAutoManual;
+
     @JsonProperty("value_auto_manual")
     private Boolean valueAutoManual;
+
+    @JsonProperty("value_command_local_remote")
+    private Boolean valueCommandLocalRemote;
 
     @JsonProperty("value_local_remote")
     private Boolean valueLocalRemote;
